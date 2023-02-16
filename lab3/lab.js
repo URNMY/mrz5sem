a8a06eb7acb600adb66af44a6bcde6d6ca1ae59e
German Osenenko <71947988+URNMY@users.noreply.github.com>
2023-02-17 01:13:07 +0300

    Update lab.js

--- lab3/lab.js
+++ lab3/lab.js
@@ -1,8 +1,7 @@
 const mathModule = (() => {
     function matrixMultiply(A, B)
     {
-        const rowsA = A.length,
-            rowsB = B.length, columnsB = B[0].length,
+        const rowsA = A.length, rowsB = B.length, columnsB = B[0].length,
             C = [];
         for (let i = 0; i < rowsA; i++) C[i] = [];
         for (let k = 0; k < columnsB; k++) {
@@ -15,56 +14,31 @@ const mathModule = (() => {
         return C;
     }
 
-    function differenceMatrices(matrix1, matrix2) {
-        const result = [];
-        const height = matrix1.length;
-        const width = matrix1[0].length;
-        for (let i = 0; i < height; i++) {
-            result.push(new Array(width));
-            for(let j = 0; j < width; j++) {
-                result[i][j] = matrix1[i][j] - matrix2[i][j];
-            }
-        }
-        return result;
-    }
-
-
-    function multiplyMatrixByNumber(matrix, number) {
-        const newMatrix = new Array(matrix.length).fill().map(el => new Array(matrix[0].length));
-        for (let i = 0; i < matrix.length; i++) {
-            for (let j = 0; j < matrix[0].length; j++) {
-                newMatrix[i][j] = matrix[i][j] * number;
-            }
-        }
-        return newMatrix;
-    }
-
     function getRandMatrix(height, width) {
-        const bottomLine = -1, topLine = 1;
+        let bottomLine = -1, topLine = 1;
         const matrix = new Array(height).fill().map(el => new Array(width));
         for (let i = 0; i < height; i++) {
             for (let j = 0; j < width; j++) {
-                matrix[i][j] = randNumberInRange(bottomLine, topLine);
+                matrix[i][j] = (topLine - bottomLine) * Math.random() + bottomLine;
+
             }
         }
         return matrix;
     }
 
-    function randNumberInRange(leftBound, rightBound) {
-        return (rightBound - leftBound) * Math.random() + leftBound;
-    }
-
-    function functionValuesToMatrixElems(matrix, func) {
-        return matrix.map(row => row.map(el => func(el)));
-    }
-
     function sumMatrix(matrix1, matrix2) {
-        return differenceMatrices(matrix1, multiplyMatrixByNumber(matrix2, -1));
+        const result = [];
+        for (let i = 0; i < matrix1.length; i++) {
+            result.push(new Array(matrix1[0].length));
+            for(let j = 0; j < matrix1[0].length; j++) {
+                result[i][j] = matrix1[i][j] + matrix2[i][j];
+            }
+        }
+        return result;
     }
     return {
         matrixMultiply,
         getRandMatrix,
-        functionValuesToMatrixElems,
         sumMatrix
     }
 })();
@@ -73,29 +47,29 @@ const leakyReLUModule = (() => {
     const contextNeuronActivationFunction = Math.asinh;
     const hiddenNeuronActivationFunction = (arg) => arg;
     const outputNeuronActivationFunction = (arg) => arg;
-    const hiddenNeuronActivationDerivativeFunction = (arg) => 1;
-    const inputLayerNeuronsQuantity = 5;
-    const hiddenLayerNeuronsQuantity = 1;
-    const outputLayerNeuronsQuantity = 1;
-    const contextNeuronsQuantity = outputLayerNeuronsQuantity;
-    const weights = {};
+    const hiddenNeuronActivationDerivativeFunction = () => 1;
+    const inputLayerNeuronsCount = 5;
+    const hiddenLayerNeuronsCount = 1;
+    const outputLayerNeuronsCount = 1;
+    const contextNeuronsCount = outputLayerNeuronsCount;
+    const synapsesWeights = {};
     let learningParameters = {}
     let currentNotHiddenNeuronsValues;
-    let initialized = false;
-    let predictedOnce = false;
+    let isInitialized = false;
+    let isPredicted = false;
 
     function init() {
-        weights.inputToHidden = mathModule.getRandMatrix(inputLayerNeuronsQuantity, hiddenLayerNeuronsQuantity);
-        weights.hiddenLayer = mathModule.getRandMatrix(hiddenLayerNeuronsQuantity, outputLayerNeuronsQuantity);
-        weights.noHiddenToHidden = mathModule.getRandMatrix(contextNeuronsQuantity, hiddenLayerNeuronsQuantity);
+        synapsesWeights.inputToHidden = mathModule.getRandMatrix(inputLayerNeuronsCount, hiddenLayerNeuronsCount);
+        synapsesWeights.hiddenLayer = mathModule.getRandMatrix(hiddenLayerNeuronsCount, outputLayerNeuronsCount);
+        synapsesWeights.noHiddenToHidden = mathModule.getRandMatrix(contextNeuronsCount, hiddenLayerNeuronsCount);
         learningParameters = {
             currentError: 0,
-            hiddenLayerPrejudices: new Array(hiddenLayerNeuronsQuantity).fill(0),
-            maxError: Number(prompt('Input max error:')),
+            hiddenLayerPrejudices: new Array(hiddenLayerNeuronsCount).fill(0),
+            limitError: Number(prompt('Input error limit:')),
             learningCoef: Number(prompt('Input learning coefficient'))
         }
-        currentNotHiddenNeuronsValues = [new Array(contextNeuronsQuantity).fill(0)];
-        initialized = true;
+        currentNotHiddenNeuronsValues = [new Array(contextNeuronsCount).fill(0)];
+        isInitialized = true;
     }
 
     function calculateNetworkOutput(inputValues, standardValue) {
@@ -105,30 +79,24 @@ const leakyReLUModule = (() => {
         learningParameters.currStandard = standard;
 
         const hiddenLayerNeuronValues =  mathModule.sumMatrix(
-            mathModule.matrixMultiply(input, weights.inputToHidden),
-            mathModule.matrixMultiply(currentNotHiddenNeuronsValues, weights.noHiddenToHidden)
+            mathModule.matrixMultiply(input, synapsesWeights.inputToHidden),
+            mathModule.matrixMultiply(currentNotHiddenNeuronsValues, synapsesWeights.noHiddenToHidden)
         ).map((row, indexRow) => row.map(el => hiddenNeuronActivationFunction(el, learningParameters.hiddenLayerPrejudices[indexRow])));
-        const outputLayerNeuronValues = mathModule.functionValuesToMatrixElems(
-            mathModule.matrixMultiply(
+        const outputLayerNeuronValues = mathModule.matrixMultiply(
                 hiddenLayerNeuronValues,
-                weights.hiddenLayer
-            ),
-            outputNeuronActivationFunction
-        );
-        currentNotHiddenNeuronsValues = mathModule.functionValuesToMatrixElems(
-            outputLayerNeuronValues,
-            contextNeuronActivationFunction
-        );
+                synapsesWeights.hiddenLayer
+                ).map(row => row.map(el => outputNeuronActivationFunction(el)));
+        currentNotHiddenNeuronsValues = outputLayerNeuronValues.map(row => row.map(el => contextNeuronActivationFunction(el)));
 
         learningParameters.hiddenLayerNeuronValues = hiddenLayerNeuronValues;
         learningParameters.outputLayerNeuronValues = outputLayerNeuronValues;
         learningParameters.currentError = outputLayerNeuronValues[0][0] - standard;
-        predictedOnce = true;
+        isPredicted = true;
         console.log(outputLayerNeuronValues[0][0]);
     }
 
     function learn() {
-        while (Math.abs(learningParameters.currentError) > learningParameters.maxError) {
+        while (Math.abs(learningParameters.currentError) > learningParameters.limitError) {
             tuneWeights();
             tunePrejudices(learningParameters.differencingCoef);
             calculateNetworkOutput(learningParameters.initSequence, learningParameters.currStandard);
@@ -136,26 +104,26 @@ const leakyReLUModule = (() => {
     }
 
     function tuneWeights() {
-        weights.hiddenLayer = weights.hiddenLayer.map((row, indexRow) => {
+        synapsesWeights.hiddenLayer = synapsesWeights.hiddenLayer.map((row, indexRow) => {
             const differencing = learningParameters.learningCoef * learningParameters.currentError * learningParameters.hiddenLayerNeuronValues[indexRow][0];
             return row.map(el => el - differencing);
         });
 
         learningParameters.differencingCoef = learningParameters.hiddenLayerNeuronValues.map((row, indexRow) =>
-            learningParameters.learningCoef * ithGamma(learningParameters.currentError, indexRow) * hiddenNeuronActivationDerivativeFunction(row[0])
+            learningParameters.learningCoef * learningParameters.currentError * synapsesWeights.hiddenLayer[indexRow][0] * hiddenNeuronActivationDerivativeFunction()
         );
 
         const differencingCoef = learningParameters.differencingCoef;
 
-        for (let k = 0; k < weights.inputToHidden.length; k++) {
+        for (let k = 0; k < synapsesWeights.inputToHidden.length; k++) {
             for (let i = 0; i < differencingCoef.length; i++) {
-                weights.inputToHidden[k][i] -= differencingCoef[i] * learningParameters.initSequence[0][k];
+                synapsesWeights.inputToHidden[k][i] -= differencingCoef[i] * learningParameters.initSequence[0][k];
             }
         }
 
-        for (let l = 0; l < weights.noHiddenToHidden.length; l++) {
+        for (let l = 0; l < synapsesWeights.noHiddenToHidden.length; l++) {
             for (let i = 0; i < differencingCoef.length; i++) {
-                weights.noHiddenToHidden[l][i] -= differencingCoef[i] * learningParameters.hiddenLayerNeuronValues[i][0];
+                synapsesWeights.noHiddenToHidden[l][i] -= differencingCoef[i] * learningParameters.hiddenLayerNeuronValues[i][0];
             }
         }
     }
@@ -164,44 +132,27 @@ const leakyReLUModule = (() => {
         learningParameters.hiddenLayerPrejudices = learningParameters.hiddenLayerPrejudices.map((prejudice, index) => prejudice + differencingCoef[index])
     }
 
-    function ithGamma(currentError, index) {
-        return currentError * weights.hiddenLayer[index][0];
-    }
-
-    function isInitialized() {
-        return initialized;
-    }
-
     function predicted() {
-        return predictedOnce;
+        return isPredicted;
     }
 
     return {
         init,
         calculateNetworkOutput,
         learn,
-        isInitialized,
         predicted
     }
 })();
 
-document.getElementById('init-reset').addEventListener('click', () => {
+document.getElementById('button--reset').addEventListener('click', () => {
     leakyReLUModule.init();
 });
 
-document.getElementById('predict').addEventListener('click', () => {
-    if (!leakyReLUModule.isInitialized()) {
-        alert('Network is not initialized!');
-        return;
-    }
+document.getElementById('button-predict').addEventListener('click', () => {
     leakyReLUModule.calculateNetworkOutput();
 });
 
-document.getElementById('learn').addEventListener('click', () => {
-    if (!leakyReLUModule.isInitialized()) {
-        alert('Network is not initialized!');
-        return;
-    }
+document.getElementById('button-learn').addEventListener('click', () => {
     if (!leakyReLUModule.predicted()) {
         alert('You need to predict at least once before learning');
         return;
