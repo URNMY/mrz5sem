97d219df55012f260b2b70b4e2590d38c1678517
German Osenenko <71947988+URNMY@users.noreply.github.com>
2023-02-19 23:40:32 +0300

    Update lab.js

--- lab3/lab.js
+++ lab3/lab.js
@@ -1,18 +1,16 @@
-const contextNeuronActivationFunction = Math.asinh;
-const hiddenNeuronActivationFunction = (arg) => arg;
-const outputNeuronActivationFunction = (arg) => arg;
-const hiddenNeuronActivationDerivativeFunction = () => 1;
-const inputLayerNeuronsCount = 5;
-const hiddenLayerNeuronsCount = 1;
-const outputLayerNeuronsCount = 1;
-const contextNeuronsCount = outputLayerNeuronsCount;
-const synapsesWeights = {};
-let learningParameters = {}
-let currentNotHiddenNeuronsValues;
-let isInitialized = false;
-let isPredicted = false;
+const contextNeuronActivationFunction = Math.asinh,
+ hiddenNeuronActivationFunction = (arg) => arg,
+ outputNeuronActivationFunction = (arg) => arg,
+ hiddenNeuronActivationDerivativeFunction = () => 1,
+ inputLayerNeuronsCount = 5,
+ hiddenLayerNeuronsCount = 1,
+ outputLayerNeuronsCount = 1,
+ contextNeuronsCount = outputLayerNeuronsCount,
+ synapsesWeights = {};
+let learningParameters = {},
+ currentNotHiddenNeuronsValues;
 
-function init() {
+document.getElementById('button--reset').addEventListener('click', () => {
     synapsesWeights.inputToHidden = getRandMatrix(inputLayerNeuronsCount, hiddenLayerNeuronsCount);
     synapsesWeights.hiddenLayer = getRandMatrix(hiddenLayerNeuronsCount, outputLayerNeuronsCount);
     synapsesWeights.noHiddenToHidden = getRandMatrix(contextNeuronsCount, hiddenLayerNeuronsCount);
@@ -23,11 +21,7 @@ function init() {
         learningCoef: Number(prompt('Input learning coefficient'))
     }
     currentNotHiddenNeuronsValues = [new Array(contextNeuronsCount).fill(0)];
-    isInitialized = true;
-}
 
-document.getElementById('button--reset').addEventListener('click', () => {
-    init();
 });
 
 document.getElementById('button-predict').addEventListener('click', () => {
@@ -35,82 +29,59 @@ document.getElementById('button-predict').addEventListener('click', () => {
 });
 
 function calculateNetworkOutput(inputValues, standardValue) {
-    const input = inputValues ?? [prompt('Input 5 numbers').trim().split(' ').map(el => Number(el))];
-    const standard = standardValue ?? Number(prompt('Input 6th expected value'));
+    const input = inputValues ?? [prompt('Input 5 numbers').trim().split(' ').map(el => Number(el))],
+     standard = standardValue ?? Number(prompt('Input 6th expected value'));
     learningParameters.initSequence = input;
     learningParameters.currStandard = standard;
-
     const hiddenLayerNeuronValues =  sumMatrix(
         matrixMultiply(input, synapsesWeights.inputToHidden),
         matrixMultiply(currentNotHiddenNeuronsValues, synapsesWeights.noHiddenToHidden)
-    ).map((row, indexRow) => row.map(el => hiddenNeuronActivationFunction(el, learningParameters.hiddenLayerPrejudices[indexRow])));
-    const outputLayerNeuronValues = matrixMultiply(
+    ).map((row, indexRow) => row.map(el => hiddenNeuronActivationFunction(el, learningParameters.hiddenLayerPrejudices[indexRow]))),
+     outputLayerNeuronValues = matrixMultiply(
         hiddenLayerNeuronValues,
         synapsesWeights.hiddenLayer
     ).map(row => row.map(el => outputNeuronActivationFunction(el)));
     currentNotHiddenNeuronsValues = outputLayerNeuronValues.map(row => row.map(el => contextNeuronActivationFunction(el)));
-
     learningParameters.hiddenLayerNeuronValues = hiddenLayerNeuronValues;
     learningParameters.outputLayerNeuronValues = outputLayerNeuronValues;
     learningParameters.currentError = outputLayerNeuronValues[0][0] - standard;
-    isPredicted = true;
     console.log(outputLayerNeuronValues[0][0]);
 }
 
 document.getElementById('button-learn').addEventListener('click', () => {
-    if (!predicted()) {
-        alert('You need to predict at least once before learning');
-        return;
-    }
-    learn();
-});
-
-function learn() {
     while (Math.abs(learningParameters.currentError) > learningParameters.limitError) {
-        tuneWeights();
-        tunePrejudices(learningParameters.differencingCoef);
+        synapsesWeights.hiddenLayer = synapsesWeights.hiddenLayer.map((row, indexRow) => {
+            const differencing = learningParameters.learningCoef * learningParameters.currentError * learningParameters.hiddenLayerNeuronValues[indexRow][0];
+            return row.map(el => el - differencing);
+        });
+
+        learningParameters.differencingCoef = learningParameters.hiddenLayerNeuronValues.map((row, indexRow) =>
+            learningParameters.learningCoef * learningParameters.currentError * synapsesWeights.hiddenLayer[indexRow][0] * hiddenNeuronActivationDerivativeFunction()
+        );
+
+        const differencingCoef = learningParameters.differencingCoef;
+        let k = 0;
+        while (k < synapsesWeights.inputToHidden.length){
+            let i = 0;
+            while(i < differencingCoef.length) {
+                synapsesWeights.inputToHidden[k][i] = synapsesWeights.inputToHidden[k][i] - differencingCoef[i] * learningParameters.initSequence[0][k];
+                i++;
+            }
+            k++;
+        }
+        let l = 0;
+        while (l < synapsesWeights.noHiddenToHidden.length){
+            let i = 0;
+            while(i < differencingCoef.length) {
+                synapsesWeights.noHiddenToHidden[l][i] = synapsesWeights.noHiddenToHidden[l][i] - differencingCoef[i] * learningParameters.hiddenLayerNeuronValues[i][0];
+                i++;
+            }
+            k++;
+        }
+        learningParameters.hiddenLayerPrejudices = learningParameters.hiddenLayerPrejudices.map((prejudice, index) => prejudice + learningParameters.differencingCoef[index])
         calculateNetworkOutput(learningParameters.initSequence, learningParameters.currStandard);
     }
-}
-
-function tuneWeights() {
-    synapsesWeights.hiddenLayer = synapsesWeights.hiddenLayer.map((row, indexRow) => {
-        const differencing = learningParameters.learningCoef * learningParameters.currentError * learningParameters.hiddenLayerNeuronValues[indexRow][0];
-        return row.map(el => el - differencing);
-    });
-
-    learningParameters.differencingCoef = learningParameters.hiddenLayerNeuronValues.map((row, indexRow) =>
-        learningParameters.learningCoef * learningParameters.currentError * synapsesWeights.hiddenLayer[indexRow][0] * hiddenNeuronActivationDerivativeFunction()
-    );
-
-    const differencingCoef = learningParameters.differencingCoef;
-    let k = 0;
-    while (k < synapsesWeights.inputToHidden.length){
-        let i = 0;
-        while(i < differencingCoef.length) {
-            synapsesWeights.inputToHidden[k][i] -= differencingCoef[i] * learningParameters.initSequence[0][k];
-            i++;
-        }
-        k++;
-    }
-    let l = 0;
-    while (l < synapsesWeights.noHiddenToHidden.length){
-        let i = 0;
-        while(i < differencingCoef.length) {
-            synapsesWeights.noHiddenToHidden[l][i] -= differencingCoef[i] * learningParameters.hiddenLayerNeuronValues[i][0];
-            i++;
-        }
-        k++;
-    }
-}
-
-function tunePrejudices(differencingCoef) {
-    learningParameters.hiddenLayerPrejudices = learningParameters.hiddenLayerPrejudices.map((prejudice, index) => prejudice + differencingCoef[index])
-}
-
-function predicted() {
-    return isPredicted;
-}
+});
 
 function matrixMultiply(A, B)
     {
@@ -124,7 +95,7 @@ function matrixMultiply(A, B)
                 let newMatrixCell = 0;
                 let j = 0;
                 while(j < rowsB) {
-                    newMatrixCell += A[i][j]*B[j][k];
+                    newMatrixCell = newMatrixCell + A[i][j] * B[j][k];
                     j++;
                 }
                 C[i][k] = newMatrixCell;
@@ -136,7 +107,7 @@ function matrixMultiply(A, B)
     }
     function getRandMatrix(height, width) {
         let bottomLine = -1, topLine = 1;
-        const matrix = new Array(height).fill().map(el => new Array(width));
+        const matrix = new Array(height).map(el => new Array(width));
         let i = 0;
         while(i < height){
             let j = 0;
@@ -163,13 +134,3 @@ function matrixMultiply(A, B)
         }
         return result;
     }
-
-
-
-    
-
-    
-
-    
-
-
