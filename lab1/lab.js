fde751e6b42c92151972d4e84412d7d6cfc42537
German Osenenko <71947988+URNMY@users.noreply.github.com>
2023-02-19 23:10:07 +0300

    Update lab.js

--- lab1/lab.js
+++ lab1/lab.js
@@ -1,38 +1,35 @@
-const square = 4;
-const innerBlockWidth = Math.sqrt(square);
-const innerBlockHeight = Math.sqrt(square);
-const pixelsInImageSquare = innerBlockWidth * innerBlockHeight;
-const imageWidth = 256;
-const imageHeight = 256;
-const innerPixelsInOneRow = imageWidth / innerBlockWidth;
-const innerPixelsInOneColumn = imageHeight / innerBlockHeight;
-const valuesInAPixel = 3;
-const learningParameters = {};
-const compressedImageCtx = document.getElementById('compressed-image-container').getContext("2d");
-const canvas = document.getElementById("image-container");
-const ctx = canvas.getContext("2d");
-const imageInput = document.getElementById("image-input");
+const square = 4,
+     innerBlockWidth = Math.sqrt(square),
+    innerBlockHeight = Math.sqrt(square),
+     pixelsInImageSquare = innerBlockWidth * innerBlockHeight,
+    innerPixelsInOneRow = imageWidth / innerBlockWidth,
+    innerPixelsInOneColumn = imageHeight / innerBlockHeight,
+    valuesInAPixel = 3,
+    learningParameters = {},
+    imageWidth = 256,
+    imageHeight = 256,
+    maxValue = 255,
+    threshold = 0.5,
+    concisedImageContext = document.getElementById('concised-image-container').getContext("2d"),
+    canvas = document.getElementById("image-container"),
+    context = canvas.getContext("2d"),
+    imageInput = document.getElementById("area-image-input");
+
 imageInput.addEventListener("change", function() {
     const reader = new FileReader();
     reader.addEventListener("load", () => {
-        const uploadedImage = new Image();
-        uploadedImage.addEventListener("load", () => {
-            ctx.drawImage(uploadedImage, 0, 0);
-            ctx.createImageData(imageWidth, imageHeight);
+        const uploadImage = new Image();
+        uploadImage.addEventListener("load", () => {
+            context.drawImage(uploadImage, 0, 0);
+            context.createImageData(imageWidth, imageHeight);
         });
-        uploadedImage.src = reader.result;
+        uploadImage.src = reader.result;
     });
     reader.readAsDataURL(this.files[0]);
 });
 
 document.getElementById('button-compress').addEventListener('click', () => {
-    try {
         de_Compress(learningParameters);
-    }
-    catch(err) {
-        alert('DE_COMPRESSING ERROR!');
-        console.error(err);
-    }
 });
 
 function de_Compress(learningParameters) {
@@ -42,18 +39,19 @@ function de_Compress(learningParameters) {
     while(imageSquareInColumnIndex < innerPixelsInOneColumn){
         let imageSquareInRowIndex = 0;
         while(imageSquareInRowIndex < innerPixelsInOneRow) {
-            const innerPicture = getInnerPicture(imageSquareInColumnIndex, imageSquareInRowIndex);
-            const currInput = getNeuralNetworkImageSquareInput(imageSquareInColumnIndex, imageSquareInRowIndex);
-            const secondLayerValues = multiplyMatrices(currInput, weights[0]);
-            const currOutput = multiplyMatrices(secondLayerValues, weights[1]);
+            let innerPicture.x = imageSquareInRowIndex * innerBlockWidth,
+                  innerPicture.y = imageSquareInColumnIndex * innerBlockHeight,
+                  currentInput = [getColourValuesATinyPicture(innerPicture.x, innerPicture.y)],
+                  secondLayerValues = multiplyMatrices(currentInput, weights[0]),
+                  currOutput = multiplyMatrices(secondLayerValues, weights[1]);
             drawImageSquare(
-                currOutput[0].map(((2 * (colour + 1) * 255 / 2) / 255) - 1),
-                compressedImageCtx,
+                currOutput[0].map(((2 * (++colour) * maxValue / 2) / maxValue) - 1),
+                concisedImageContext,
                 innerPicture.x,
                 innerPicture.y
             )
             learningParameters.parameters.push({
-                currInput,
+                currentInput,
                 secondLayerValues,
                 currOutput,
             });
@@ -63,27 +61,15 @@ function de_Compress(learningParameters) {
     }
 }
 
-function getInnerPicture(imageSquareInColumnIndex, imageSquareInRowIndex, width = innerBlockWidth, height = innerBlockHeight) {
-    return {
-        x: imageSquareInRowIndex * width,
-        y: imageSquareInColumnIndex * height
-    };
-}
-
-function getNeuralNetworkImageSquareInput(imageSquareInColumnIndex, imageSquareInRowIndex) {
-    const innerPicture = getInnerPicture(imageSquareInColumnIndex, imageSquareInRowIndex);
-    return [getColourValuesATinyPicture(innerPicture.x, innerPicture.y)];
-}
-
-function getColourValuesATinyPicture(innerPictureX, innerPictureY, width = innerBlockWidth, height = innerBlockHeight) {
+function getColourValuesATinyPicture(innerPictureX, innerPictureY) {
     const colourValues = [];
     let j = innerPictureY;
-    while (j < innerPictureY + height) {
+    while (j < innerPictureY + innerBlockHeight) {
         let i = innerPictureX;
-        while (i < innerPictureX + width) {
-            const currentPixelData = ctx.getImageData(i, j, 1, 1).data;
+        while (i < innerPictureX + innerBlockWidth) {
+            const currentPixelData = context.getImageData(i, j, 1, 1).data;
             for (let k = 0; k < square - 1; k++) {
-                colourValues.push((2 * currentPixelData[k] / 255) - 1);
+                colourValues.push((2 * currentPixelData[k] / maxValue) - 1);
             }
             i++;
         }
@@ -117,49 +103,43 @@ function getColourValuesATinyPicture(innerPictureX, innerPictureY, width = inner
         return C;
     }
 
-function drawImageSquare(colourValues, ctx, innerPictureX, innerPictureY, width = innerBlockWidth, height = innerBlockHeight) {
-    const imageSquareData = ctx.createImageData(width, height), maxValue = 255;
-    let currColourValueIndex = 0, imageDataIndex = 0;
-    while (currColourValueIndex < colourValues.length){
+function drawImageSquare(colourValues, context, innerPictureX, innerPictureY) {
+    const imageSquareData = context.createImageData(innerBlockWidth, innerBlockHeight);
+    let colourValueIndex = 0, imageDataIndex = 0;
+    while (colourValueIndex < colourValues.length){
         for (let i = 0; i < square; i++) {
             if (i !== square - 1){
-                imageSquareData.data[imageDataIndex + i] = colourValues[currColourValueIndex + i];
+                imageSquareData.data[imageDataIndex + i] = colourValues[colourValueIndex + i];
             }
             else {
                 imageSquareData.data[imageDataIndex + i] = maxValue;
             }
         }
-        currColourValueIndex += 3;
-        imageDataIndex += 4;
+        colourValueIndex = colourValueIndex + 3;
+        imageDataIndex = imageDataIndex + 4;
     }
-    ctx.putImageData(imageSquareData, innerPictureX, innerPictureY);
+    context.putImageData(imageSquareData, innerPictureX, innerPictureY);
 }
 
 document.getElementById('button-reset').addEventListener('click', () => {
-    const secondLayerNeurons = +prompt('Count of neurons on the second layer:');
-    if (!secondLayerNeurons || isNaN(secondLayerNeurons) || secondLayerNeurons % 1 !== 0) return;
-    const errorLimit = +prompt('Error limit:');
-    if (!errorLimit || isNaN(errorLimit)) return;
-    learningParameters.currentWeights = setRandomWeights(secondLayerNeurons);
-    learningParameters.maxErr = errorLimit;
-});
-
-function setRandomWeights(secondLayerNeurons) {
-    const firstWeights = [];
+    const secondLayerNeurons = +prompt('Count of neurons on the second layer:'),
+        errorLimit = +prompt('Error limit:'),
+        firstWeights = [];
     let i = 0;
     while (i < pixelsInImageSquare * valuesInAPixel) {
         const newRow = [];
         let j = 0;
         while (j < secondLayerNeurons) {
-            newRow.push(Math.random() * (Math.random() > 0.5 ? 1 : -1));
+            newRow.push(Math.random() * (Math.random() > threshold ? 1 : -1));
             j++;
         }
         firstWeights.push(newRow);
         i++;
     }
     const secondWeights = transposingMatrix(firstWeights);
-    return [firstWeights, secondWeights];
-}
+    learningParameters.currentWeights = [firstWeights, secondWeights];
+    learningParameters.limError = errorLimit;
+});
 
 function transposingMatrix(matrix) {
     const transposing = [];
@@ -179,38 +159,49 @@ function transposingMatrix(matrix) {
 document.getElementById('button-learning-step').addEventListener('click', () => {
     let i = 0;
     while (i < learningParameters.parameters.length) {
-        tuneWeights(learningParameters.currentWeights, learningParameters.maxErr, learningParameters.parameters[i]);
+        tuningWeights(learningParameters.currentWeights, learningParameters.limError, learningParameters.parameters[i]);
         i++;
     }
     de_Compress(learningParameters);
     alert('Done!');
 });
 
-function tuneWeights(weights, maxErr, parameters) {
-    if (!weights || !parameters || !parameters.currInput || !parameters.secondLayerValues || !parameters.currOutput) return;
-    const X = parameters.currInput;
-    let Y = parameters.secondLayerValues, XOverlined = parameters.currOutput,
-        diff = differenceMatrices(XOverlined, X), currError = maxErr + 1;
-    while(currError > maxErr) {
-        const secondLayerValuesTransposing = transposingMatrix(Y);
-        const inputTransposing = transposingMatrix(X);
-        let a = multiplyMatrices(secondLayerValuesTransposing, diff);
-        let learningCoefficient = 1 / transposingMatrix(secondLayerValuesTransposing)[0].reduce((row, value) => row + value*value, 0);
-        if(!isFinite(learningCoefficient)) return;
+function tuningWeights(weights, limError, parameters) {
+    const inputLayerX = parameters.currentInput, next = 1, previous = 0;
+    let secondLayerY = parameters.secondLayerValues, XOverlined = parameters.currOutput,
+        diff = differenceMatrices(XOverlined, inputLayerX), currError = limError + 1;
+    while(currError > limError) {
+        const secondLayerValuesTransposing = transposingMatrix(secondLayerY),
+              inputTransposing = transposingMatrix(inputLayerX);
+        let a = multiplyMatrices(secondLayerValuesTransposing, diff),
+            learningCoefficient = Math.pow(transposingMatrix(secondLayerValuesTransposing)[0].reduce((row, value) => row + Math.pow(value,2), 0),-1);
         multiplyMatrixByNumber(a, learningCoefficient);
-        weights[1] = differenceMatrices(weights[1], a);
         a = multiplyMatrices(inputTransposing, diff);
-        const b = multiplyMatrices(a, transposingMatrix(weights[1]));
-        learningCoefficient = 1 / transposingMatrix(inputTransposing)[0].reduce((row, value) => row + value*value, 0)
-        if(!isFinite(learningCoefficient)) return;
+        weights[next] = differenceMatrices(weights[next], a);
+        const b = multiplyMatrices(a, transposingMatrix(weights[next]));
+        learningCoefficient = Math.pow(transposingMatrix(inputTransposing)[0].reduce((row, value) => row + Math.pow(value,2), 0),-1);
         multiplyMatrixByNumber(b, learningCoefficient);
-        weights[0] = differenceMatrices(weights[0], b);
-        weights[1].forEach((row, index) => normalizeRow(weights[1], index));
-        normalizeByColumn(weights[0]);
-        Y = multiplyMatrices(X, weights[0]);
-        XOverlined = multiplyMatrices(Y, weights[1]);
-        diff = differenceMatrices(XOverlined, X);
-        currError = diff[0].reduce((row, value) => row + value*value, 0);
+        weights[previous] = differenceMatrices(weights[previous], b);
+        weights[next].forEach((row, index) => normalizeRow(weights[next], index));
+        let j = 0;
+        while(j < weights[previous][previous].length) {
+            let columnSum = 0, i = 0;
+            while(i < weights[previous].length) {
+                columnSum += Math.pow(weights[previous][i][j], 2);
+                i++;
+            }
+            const modulus = Math.sqrt(columnSum);
+            let k = 0;
+            while(k < weights[previous].length) {
+                weights[previous][k][j] = weights[previous][k][j] / modulus;
+                k++;
+            }
+            j++;
+        }
+        secondLayerY = multiplyMatrices(inputLayerX, weights[0]);
+        XOverlined = multiplyMatrices(secondLayerY, weights[1]);
+        diff = differenceMatrices(XOverlined, inputLayerX);
+        currError = diff[0].reduce((row, value) => row + Math.pow(value,2), 0);
     }
 }
 
@@ -218,35 +209,17 @@ function normalizeRow(matrix, indexRow) {
     const modulus = Math.sqrt(matrix[indexRow].map(el => el * el).reduce((row, value) => row + value, 0));
     let i = 0;
     while(i < matrix[indexRow].length) {
-        matrix[indexRow][i] /= modulus;
+        matrix[indexRow][i] = matrix[indexRow][i] / modulus;
         i++;
     }
 }
-
-function normalizeByColumn(weightMatrix) {
-    let j = 0;
-    while(j < weightMatrix[0].length) {
-        let columnSum = 0, i = 0;
-        while(i < weightMatrix.length) {
-            columnSum += Math.pow(weightMatrix[i][j], 2);
-            i++;
-        }
-        const modulus = Math.sqrt(columnSum);
-        let k = 0;
-        while(k < weightMatrix.length) {
-            weightMatrix[k][j] /= modulus;
-            k++;
-        }
-        j++;
-    }
-}
 function differenceMatrices(A, B) {
-    const result = [], height = A.length, width = A[0].length;
+    const result = [];
     let i = 0;
-    while(i < height) {
-        result.push(new Array(width));
+    while(i < A.length) {
+        result.push(new Array(A[0].length));
         let j = 0;
-        while(j < width) {
+        while(j < A[0].length) {
             result[i][j] = A[i][j] - B[i][j];
             j++;
         }
@@ -260,7 +233,7 @@ function multiplyMatrixByNumber(matrix, number) {
     while(i < matrix.length) {
         let j = 0;
         while(j < matrix[0].length){
-            matrix[i][j] *= number;
+            matrix[i][j] = matrix[i][j] * number;
             j++;
         }
         i++;
@@ -269,12 +242,11 @@ function multiplyMatrixByNumber(matrix, number) {
 
 document.getElementById('button-learning-steps').addEventListener('click', () => {
     const steps = +prompt("Learning steps:");
-    if (!steps || isNaN(steps) || steps <= 0 || steps % 1 !== 0) return;
     let it = 0;
     while (it < steps) {
         let i = 0;
         while(i < learningParameters.parameters.length) {
-            tuneWeights(learningParameters.currentWeights, learningParameters.maxErr, learningParameters.parameters[i]);
+            tuningWeights(learningParameters.currentWeights, learningParameters.limError, learningParameters.parameters[i]);
             i++;
         }
         de_Compress(learningParameters);
